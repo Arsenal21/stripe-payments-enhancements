515c87e1de12138a2794f55f7b2c3690459e55fb
Alexander C <eminemwalsh@gmail.com>
2020-12-10 20:04:57 +0200

    Update asp-prefill-customer-name-and-email.php

--- asp-prefill-customer-name-and-email/asp-prefill-customer-name-and-email.php
+++ asp-prefill-customer-name-and-email/asp-prefill-customer-name-and-email.php
@@ -11,7 +11,7 @@ class ASP_prefill_customer_name_and_email {
 	public function __construct() {
 		add_action( 'asp-button-output-data-ready', array( $this, 'handle_data_ready' ), 10, 2 );
 	}
-	public function handle_data_ready( $data, $not_used ) {
+	public function handle_data_ready( $data ) {
 		$data['customer_email'] = 'john@example.com';
 		$data['customer_name']  = 'John Doe';
 		return $data;
